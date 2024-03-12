224c1cd0341c633acdf57d23380ce6b2c5168135
shinyo17 <17shinyo17@gmail.com>
2024-03-12 16:02:04 +0900

    init: 프론트 리뉴얼

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
+import styleX from 'vite-plugin-stylex'
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
 
 const config: UserConfig = {
-  plugins: [react(), ssr()]
+  plugins: [
+    react({
+      babel: {
+        plugins: [jotaiDebugLabel, jotaiReactRefresh],
+        // https://jotai.org/docs/guides/vite
+      },
+    }),
+    ssr(),
+    styleX(),
+  ],
 }
 
 export default config
